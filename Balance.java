2e4b0e22e06f19cadd7494c29816544399fe4e4d
Chaitanya <141550576+XChaitanyaX@users.noreply.github.com>
2024-03-16 11:10:39 +0530

    Update Balance.java

--- Balance.java
+++ Balance.java
@@ -1,30 +1,5 @@
-import java.util.*;
-class Balance
-{
-    public static void main(String args[])
-    {
-        Scanner input=new Scanner(System.in);
-        System.out.print("Enter the amount:");
-        double amount=input.nextDouble();
-        double interest;
-        double rate;
-        for(int i=1;i<=12;i++)
-        {
-            if (amount<=110000)
-            {
-                rate=0.01;
-            }
-            else if (amount>110000 && amount<=125000)
-            {
-                rate=0.015;
-            }
-            else
-            {
-                rate=0.02;
-            }
-            interest=amount*rate;
-            amount=amount+interest+1000;
-            System.out.println(i+"\t"+rate+"\t"+interest+"\t"+amount);
-        }
+class new{
+    public static void main(String args[]){
+        System.out.println("Hello");
     }
 }
